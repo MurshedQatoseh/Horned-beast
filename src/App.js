04a7b1561b7be10fe6dc111bb892bfd77fd1e3f8
Murshed Majdy Qatoseh <morshedmorahed01@gmail.com>
2022-08-25 13:08:25 +0300

    lab02

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from "react";
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Main from './components/Main';
 
-class App extends React.Component {
-    render() {
-        return(
-            <div>
-                <Header />
-                <div className="cards">   
-                <Main />
-                </div>
-                <Footer />
-            </div>
-        )
-    }
+import './App.css';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Main from './component/Main';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+
+        <Header/>
+        
+        <Main/>
+        
+
+        <Footer/>
+        
+     
+    </div>
+  );
 }
 
 export default App;
